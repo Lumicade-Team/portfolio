6ee9707629a9e80329ff9e0df4cea79ef4390f01
Danish Danial <danishdanialzurkanain@gmail.com>
2026-03-27 22:40:54 +0800

    ✨Register number

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -30,8 +30,14 @@ const Footer = () => {
                     rel="noopener noreferrer"
                     className="mr-6 text-body-color-dark duration-300 hover:text-primary"
                   >
-                    <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2Zm0 4.236-8 5-8-5V6l8 5 8-5v2.236Z"/>
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M20 4H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2Zm0 4.236-8 5-8-5V6l8 5 8-5v2.236Z" />
                     </svg>
                   </a>
                   <a
@@ -41,8 +47,17 @@ const Footer = () => {
                     rel="noopener noreferrer"
                     className="mr-6 text-body-color-dark duration-300 hover:text-primary"
                   >
-                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M16.5 0h-15A1.5 1.5 0 000 1.5v15A1.5 1.5 0 001.5 18h15a1.5 1.5 0 001.5-1.5v-15A1.5 1.5 0 0016.5 0zM5.25 15H2.625V6.75H5.25V15zm-1.312-9A1.312 1.312 0 113.938 4.375 1.312 1.312 0 013.938 6zm11.062 9h-2.625v-4.125c0-.984-.016-2.25-1.375-2.25-1.375 0-1.584 1.078-1.584 2.188V15H6.791V6.75h2.5v1.125h.035a2.75 2.75 0 012.48-1.375c2.656 0 3.146 1.75 3.146 4.031V15z" fill="currentColor"/>
+                    <svg
+                      width="18"
+                      height="18"
+                      viewBox="0 0 18 18"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M16.5 0h-15A1.5 1.5 0 000 1.5v15A1.5 1.5 0 001.5 18h15a1.5 1.5 0 001.5-1.5v-15A1.5 1.5 0 0016.5 0zM5.25 15H2.625V6.75H5.25V15zm-1.312-9A1.312 1.312 0 113.938 4.375 1.312 1.312 0 013.938 6zm11.062 9h-2.625v-4.125c0-.984-.016-2.25-1.375-2.25-1.375 0-1.584 1.078-1.584 2.188V15H6.791V6.75h2.5v1.125h.035a2.75 2.75 0 012.48-1.375c2.656 0 3.146 1.75 3.146 4.031V15z"
+                        fill="currentColor"
+                      />
                     </svg>
                   </a>
                   <a
@@ -52,8 +67,17 @@ const Footer = () => {
                     rel="noopener noreferrer"
                     className="mr-6 text-body-color-dark duration-300 hover:text-primary"
                   >
-                    <svg width="18" height="18" viewBox="0 0 1200 1227" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M1199.61 0H950.09L600.01 462.09L249.91 0H0L462.09 629.13L0 1227H249.91L600.01 764.91L950.09 1227H1200L737.91 597.87L1199.61 0ZM900.09 1113.6L600.01 713.04L299.91 1113.6H149.91L600.01 484.5L1050.09 1113.6H900.09Z" fill="currentColor"/>
+                    <svg
+                      width="18"
+                      height="18"
+                      viewBox="0 0 1200 1227"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M1199.61 0H950.09L600.01 462.09L249.91 0H0L462.09 629.13L0 1227H249.91L600.01 764.91L950.09 1227H1200L737.91 597.87L1199.61 0ZM900.09 1113.6L600.01 713.04L299.91 1113.6H149.91L600.01 484.5L1050.09 1113.6H900.09Z"
+                        fill="currentColor"
+                      />
                     </svg>
                   </a>
                   <a
@@ -63,9 +87,23 @@ const Footer = () => {
                     rel="noopener noreferrer"
                     className="mr-6 text-body-color-dark duration-300 hover:text-primary"
                   >
-                    <svg width="18" height="18" viewBox="0 0 448 448" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <rect width="448" height="448" rx="96" fill="currentColor"/>
-                      <path d="M224 144c-44.2 0-80 35.8-80 80s35.8 80 80 80 80-35.8 80-80-35.8-80-80-80zm0 128c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm88-132c0 10.7-8.7 19.4-19.4 19.4-10.7 0-19.4-8.7-19.4-19.4s8.7-19.4 19.4-19.4c10.7 0 19.4 8.7 19.4 19.4z" fill="#142336"/>
+                    <svg
+                      width="18"
+                      height="18"
+                      viewBox="0 0 448 448"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect
+                        width="448"
+                        height="448"
+                        rx="96"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M224 144c-44.2 0-80 35.8-80 80s35.8 80 80 80 80-35.8 80-80-35.8-80-80-80zm0 128c-26.5 0-48-21.5-48-48s21.5-48 48-48 48 21.5 48 48-21.5 48-48 48zm88-132c0 10.7-8.7 19.4-19.4 19.4-10.7 0-19.4-8.7-19.4-19.4s8.7-19.4 19.4-19.4c10.7 0 19.4 8.7 19.4 19.4z"
+                        fill="#142336"
+                      />
                     </svg>
                   </a>
                   <a
@@ -75,9 +113,23 @@ const Footer = () => {
                     rel="noopener noreferrer"
                     className="mr-6 text-body-color-dark duration-300 hover:text-primary"
                   >
-                    <svg width="18" height="18" viewBox="0 0 448 448" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <rect width="448" height="448" rx="96" fill="currentColor"/>
-                      <path d="M186.8 304.6l123.7-64.6c7.7-4 7.7-15.6 0-19.6l-123.7-64.6c-7.7-4-17.1 1.2-17.1 9.8v129.2c0 8.6 9.4 13.8 17.1 9.8z" fill="#142336"/>
+                    <svg
+                      width="18"
+                      height="18"
+                      viewBox="0 0 448 448"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect
+                        width="448"
+                        height="448"
+                        rx="96"
+                        fill="currentColor"
+                      />
+                      <path
+                        d="M186.8 304.6l123.7-64.6c7.7-4 7.7-15.6 0-19.6l-123.7-64.6c-7.7-4-17.1 1.2-17.1 9.8v129.2c0 8.6 9.4 13.8 17.1 9.8z"
+                        fill="#142336"
+                      />
                     </svg>
                   </a>
                 </div>
@@ -190,7 +242,11 @@ const Footer = () => {
           <div className="h-px w-full bg-linear-to-r from-transparent via-lumi-mutednav to-transparent"></div>
           <div className="py-8">
             <p className="text-center text-base text-body-color-dark">
-              &copy; {new Date().getFullYear()} Lumicade Solutions. All rights reserved.
+              &copy; {new Date().getFullYear()} Lumicade Solutions. All rights
+              reserved.
+            </p>
+            <p className="text-center text-body-color-dark text-sm">
+              JM1042046-K
             </p>
           </div>
         </div>
