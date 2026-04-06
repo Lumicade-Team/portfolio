73c9b37eeb2920aef9f8fc637baf0c70a9d0e499
Danish Danial <danishdanialzurkanain@gmail.com>
2026-04-06 19:00:54 +0800

    ✨My bro suck a D last Sunday

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,7 @@
+"use client";
+
 import AboutSectionOne from "@/components/About/AboutSectionOne";
 import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "About | Lumicade Solutions",
-  description: "Lumicade Solutions is a Malaysia-based software house and SaaS provider. We research, design, build, and deploy IT solutions across multiple domains.",
-};
 
 const AboutPage = () => {
   return (
